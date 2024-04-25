9fbf1a7198223e1d2c68485f88ca25278c22c118
TwanstCodess <sulltantwanst@gmail.com>
2024-04-26 00:24:49 +0300

    comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import User from './components/user';
+import Login from './components/login';
 const App=()=>{
   return(
     <div>
       <User/>
+      <Login/>
     </div>
   )
 }
---
+++ src/components/login.js
@@ -0,0 +1,10 @@
+const login=()=>{
+    return(
+        <div>
+        <input type="text" placeholder="Enter your name"/>
+        <input type="text" placeholder="Enter your password"/>
+        <button>Login</button>
+        </div>  
+    )
+}
+export default login
