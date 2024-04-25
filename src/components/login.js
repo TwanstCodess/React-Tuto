7226b3279111560e120abd427377c8728242ace3
TwanstCodess <sulltantwanst@gmail.com>
2024-04-26 00:52:31 +0300

    nfn

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,17 @@
 const login=()=>{
+    const first = (name) => { 
+        console.log("Hi "+ name)
+     }
     return(
         <div>
         <input  type="text" placeholder="Enter your name"/>
         <input type="text" placeholder="Enter your password"/>
-        <button>Login</button>
+        <button onClick={
+            // sudi ananamas function bo function parimtara zor sudfi haia ba kare bena agar paramitari nabu awa ba kare ma hene
+            ()=>{
+              first("sara")
+            }
+        }>Login</button>
         </div>  
     )
 }
